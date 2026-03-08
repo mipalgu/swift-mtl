304eea6426c204dfeca73b7eff477e5924f5c604
Rene Hexel <r.hexel@griffith.edu.au>
2026-03-08 11:55:27 +1000

    Use stable swift-ecore and swift-aql releases

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-collections", from: "1.0.0"),
-        .package(url: "https://github.com/mipalgu/swift-ecore", branch: "main"),
-        .package(url: "https://github.com/mipalgu//swift-aql", branch: "main"),
+        .package(url: "https://github.com/mipalgu/swift-ecore", from: "0.1.1"),
+        .package(url: "https://github.com/mipalgu/swift-aql", from: "0.1.4"),
     ],
     targets: [
         .target(
